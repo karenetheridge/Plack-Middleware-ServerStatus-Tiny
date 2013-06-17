91457a7178dd36471151d0bd1903700787c4ebe7
Karen Etheridge <ether@cpan.org>
2013-06-17 13:55:16 -0700

    initial commit, via [Git::Init] in Dist-Zilla-PluginBundle-Author-ETHER-0.008, github profile.

---
+++ lib/Plack/Middleware/ServerStatus/Tiny.pm
@@ -0,0 +1,53 @@
+use strict;
+use warnings;
+package Plack::Middleware::ServerStatus::Tiny;
+# ABSTRACT: ...
+
+
+
+1;
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+    use Plack::Middleware::ServerStatus::Tiny;
+
+    ...
+
+=head1 DESCRIPTION
+
+...
+
+=head1 FUNCTIONS/METHODS
+
+=over 4
+
+=item * C<foo>
+
+...
+
+=back
+
+=head1 SUPPORT
+
+=for stopwords irc
+
+Bugs may be submitted through L<the RT bug tracker|https://rt.cpan.org/Public/Dist/Display.html?Name=Plack-Middleware-ServerStatus-Tiny>
+(or L<bug-Plack-Middleware-ServerStatus-Tiny@rt.cpan.org|mailto:bug-Plack-Middleware-ServerStatus-Tiny@rt.cpan.org>).
+I am also usually active on irc, as 'ether' at C<irc.perl.org>.
+
+=head1 ACKNOWLEDGEMENTS
+
+...
+
+=head1 SEE ALSO
+
+=begin :list
+
+* L<foo>
+
+=end :list
+
+=cut
