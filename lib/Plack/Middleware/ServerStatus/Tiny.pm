7b050793fb0551d724cd7ce572f5527948a17987
Karen Etheridge <ether@cpan.org>
2013-06-17 16:27:52 -0700

    docs

--- lib/Plack/Middleware/ServerStatus/Tiny.pm
+++ lib/Plack/Middleware/ServerStatus/Tiny.pm
@@ -1,7 +1,7 @@
 use strict;
 use warnings;
 package Plack::Middleware::ServerStatus::Tiny;
-# ABSTRACT: ...
+# ABSTRACT: tiny middleware for providing server status information
 
 use parent 'Plack::Middleware';
 use Plack::Util::Accessor qw(path _uptime _access_count);
@@ -45,21 +45,35 @@ __END__
 
 =head1 SYNOPSIS
 
-    use Plack::Middleware::ServerStatus::Tiny;
+    use Plack::Builder;
 
-    ...
+    builder {
+        enable 'ServerStatus::Tiny', path => '/status';
+        $app;
+    };
+
+    $ curl http://server:port/status
+    uptime: 120; access count: 10 (this process only)
 
 =head1 DESCRIPTION
 
-...
+This middleware is extremely lightweight: faster and smaller than
+L<Plack::Middleware::ServerStatus::Lite>. While that middleware is useful for
+showing the status of all workers, their pids and their last requests, it can
+be a bit heavy for frequent pinging (for example by a load balancer to confirm
+that the server is still up).
 
-=head1 FUNCTIONS/METHODS
+This middleware does not use the disk, keeping all its data in memory in the
+worker process.  All it returns is the number of seconds since the last server
+restart, and how many requests this particular process has serviced.
+
+=head1 CONFIGURATION
 
 =over 4
 
-=item * C<foo>
+=item * C<path>
 
-...
+The path which returns the server status.
 
 =back
 
@@ -71,15 +85,11 @@ Bugs may be submitted through L<the RT bug tracker|https://rt.cpan.org/Public/Di
 (or L<bug-Plack-Middleware-ServerStatus-Tiny@rt.cpan.org|mailto:bug-Plack-Middleware-ServerStatus-Tiny@rt.cpan.org>).
 I am also usually active on irc, as 'ether' at C<irc.perl.org>.
 
-=head1 ACKNOWLEDGEMENTS
-
-...
-
 =head1 SEE ALSO
 
 =begin :list
 
-* L<foo>
+* L<Plack::Middleware::ServerStatus::Lite>
 
 =end :list
 
